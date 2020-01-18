1cd4587c49436803c42676ffdbb0c8be5112414f
Aleksandr Petrukhin <razblade@gmail.com>
2020-01-18 23:10:57 +0300

    Update app.component.html, policies.component.html, and policies.component.ts

--- src/app/policies/policies.component.ts
+++ src/app/policies/policies.component.ts
@@ -24,9 +24,17 @@ export class PoliciesComponent implements OnInit {
   modalCreateEditTitle;
   modalCreateEditButtonText;
 
-  dropdownList = [];
-  selectedItems = [];
-  dropdownSettings = {};
+  dropdownActionList = [];
+  dropdownConditionList = [];
+  dropdownConditionKeyList = [];
+
+  selectedActions = [];
+  selectedCondition = "";
+  selectedConditionKey = "";
+
+  dropdownActionSettings = {};
+  dropdownConditionSettings = {};
+  dropdownConditionKeySettings = {};
 
   newPolicy = {
   	name:"",
@@ -42,8 +50,12 @@ export class PoliciesComponent implements OnInit {
   newStatement = {
     Action: [],
     Effect: "",
-    Resource: []
+    Resource: [],
+    Condition: {},
+    Principal: ""
   }
+
+  newConditionValue = ""
 
   @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
   @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
@@ -67,14 +79,14 @@ export class PoliciesComponent implements OnInit {
         }
       }
     }
-    
+
   }
 
   ngOnInit() {
   	this.getPolicies()
 
 
-  	this.dropdownList = [
+  	this.dropdownActionList = [
 	    {"id":1,"itemName":"s3:AbortMultipartUpload"},
 	    {"id":2,"itemName":"s3:CreateBucket"},
 	    {"id":3,"itemName":"s3:DeleteBucket"},
@@ -97,29 +109,200 @@ export class PoliciesComponent implements OnInit {
 	    {"id":20,"itemName":"s3:GetBucketLifecycle"}
     ];
 
-    this.dropdownSettings = { 
-		singleSelection: false, 
-		text:"Select Actions",
-		selectAllText:'Select All',
-		unSelectAllText:'UnSelect All',
-		enableSearchFilter: true	
-	  };            
+    this.dropdownActionSettings = {
+  		singleSelection: false,
+  		text:"Select Actions",
+  		selectAllText:'Select All',
+  		unSelectAllText:'UnSelect All',
+  		enableSearchFilter: true
+	  };
+
+  	this.dropdownConditionList = [
+      {"id":1,"itemName":"ArnEquals"},
+      {"id":2,"itemName":"ArnEqualsIfExists"},
+      {"id":3,"itemName":"ArnLike"},
+      {"id":4,"itemName":"ArnLikeIfExists"},
+      {"id":5,"itemName":"ArnNotEquals"},
+      {"id":6,"itemName":"ArnNotEqualsIfExists"},
+      {"id":7,"itemName":"ArnNotLike"},
+      {"id":8,"itemName":"ArnNotLikeIfExists"},
+      {"id":9,"itemName":"BinaryEquals"},
+      {"id":10,"itemName":"BinaryEqualsIfExists"},
+      {"id":11,"itemName":"BinaryNotEquals"},
+      {"id":12,"itemName":"BinaryNotEqualsIfExists"},
+      {"id":13,"itemName":"Bool"},
+      {"id":14,"itemName":"BoolIfExists"},
+      {"id":15,"itemName":"DateEquals"},
+      {"id":16,"itemName":"DateEqualsIfExists"},
+      {"id":17,"itemName":"DateGreaterThan"},
+      {"id":18,"itemName":"DateGreaterThanEquals"},
+      {"id":19,"itemName":"DateGreaterThanEqualsIfExists"},
+      {"id":20,"itemName":"DateGreaterThanIfExists"},
+      {"id":21,"itemName":"DateLessThan"},
+      {"id":22,"itemName":"DateLessThanEquals"},
+      {"id":23,"itemName":"DateLessThanEqualsIfExists"},
+      {"id":24,"itemName":"DateLessThanIfExists"},
+      {"id":25,"itemName":"DateNotEquals"},
+      {"id":26,"itemName":"DateNotEqualsIfExists"},
+      {"id":27,"itemName":"IpAddress"},
+      {"id":28,"itemName":"IpAddressIfExists"},
+      {"id":29,"itemName":"NotIpAddress"},
+      {"id":30,"itemName":"NotIpAddressIfExists"},
+      {"id":31,"itemName":"Null"},
+      {"id":32,"itemName":"NumericEquals"},
+      {"id":33,"itemName":"NumericEqualsIfExists"},
+      {"id":34,"itemName":"NumericGreaterThan"},
+      {"id":35,"itemName":"NumericGreaterThanEquals"},
+      {"id":36,"itemName":"NumericGreaterThanEqualsIfExists"},
+      {"id":37,"itemName":"NumericGreaterThanIfExists"},
+      {"id":38,"itemName":"NumericLessThan"},
+      {"id":39,"itemName":"NumericLessThanEquals"},
+      {"id":40,"itemName":"NumericLessThanEqualsIfExists"},
+      {"id":41,"itemName":"NumericLessThanIfExists"},
+      {"id":42,"itemName":"NumericNotEquals"},
+      {"id":43,"itemName":"NumericNotEqualsIfExists"},
+      {"id":44,"itemName":"StringEquals"},
+      {"id":45,"itemName":"StringEqualsIfExists"},
+      {"id":46,"itemName":"StringEqualsIgnoreCase"},
+      {"id":47,"itemName":"StringEqualsIgnoreCaseIfExists"},
+      {"id":48,"itemName":"StringLike"},
+      {"id":49,"itemName":"StringLikeIfExists"},
+      {"id":50,"itemName":"StringNotEquals"},
+      {"id":51,"itemName":"StringNotEqualsIfExists"},
+      {"id":52,"itemName":"StringNotEqualsIgnoreCase"},
+      {"id":53,"itemName":"StringNotEqualsIgnoreCaseIfExists"},
+      {"id":54,"itemName":"StringNotLike"},
+      {"id":55,"itemName":"StringNotLikeIfExists"}
+    ];
+
+    this.dropdownConditionSettings = {
+  		singleSelection: true,
+  		text:"Select Condition",
+  		selectAllText:'Select All',
+  		unSelectAllText:'UnSelect All',
+  		enableSearchFilter: true
+	  };
+
+  	this.dropdownConditionKeyList = [
+      {"id":1,"itemName":"aws:CurrentTime"},
+      {"id":2,"itemName":"aws:EpochTime"},
+      {"id":3,"itemName":"aws:MultiFactorAuthAge"},
+      {"id":4,"itemName":"aws:MultiFactorAuthPresent"},
+      {"id":5,"itemName":"aws:PrincipalArn"},
+      {"id":6,"itemName":"aws:PrincipalOrgID"},
+      {"id":7,"itemName":"aws:PrincipalTag/${TagKey}"},
+      {"id":8,"itemName":"aws:PrincipalType"},
+      {"id":9,"itemName":"aws:Referer"},
+      {"id":10,"itemName":"aws:RequestTag/${TagKey}"},
+      {"id":11,"itemName":"aws:RequestedRegion"},
+      {"id":12,"itemName":"aws:SecureTransport"},
+      {"id":13,"itemName":"aws:SourceAccount"},
+      {"id":14,"itemName":"aws:SourceArn"},
+      {"id":15,"itemName":"aws:SourceIp"},
+      {"id":16,"itemName":"aws:SourceVpc"},
+      {"id":17,"itemName":"aws:SourceVpce"},
+      {"id":18,"itemName":"aws:TagKeys"},
+      {"id":19,"itemName":"aws:TokenIssueTime"},
+      {"id":20,"itemName":"aws:UserAgent"},
+      {"id":21,"itemName":"aws:userid"},
+      {"id":22,"itemName":"aws:username"},
+      {"id":23,"itemName":"s3:AccessPointNetworkOrigin"},
+      {"id":24,"itemName":"s3:DataAccessPointAccount"},
+      {"id":25,"itemName":"s3:DataAccessPointArn"},
+      {"id":26,"itemName":"s3:ExistingJobOperation"},
+      {"id":27,"itemName":"s3:ExistingJobPriority"},
+      {"id":28,"itemName":"s3:ExistingObjectTag/<key>"},
+      {"id":29,"itemName":"s3:JobSuspendedCause"},
+      {"id":30,"itemName":"s3:LocationConstraint"},
+      {"id":31,"itemName":"s3:RequestJobOperation"},
+      {"id":32,"itemName":"s3:RequestJobPriority"},
+      {"id":33,"itemName":"s3:RequestObjectTag/<key>"},
+      {"id":34,"itemName":"s3:RequestObjectTagKeys"},
+      {"id":35,"itemName":"s3:VersionId"},
+      {"id":36,"itemName":"s3:authtype"},
+      {"id":37,"itemName":"s3:delimiter"},
+      {"id":38,"itemName":"s3:locationconstraint"},
+      {"id":39,"itemName":"s3:max-keys"},
+      {"id":40,"itemName":"s3:object-lock-legal-hold"},
+      {"id":41,"itemName":"s3:object-lock-mode"},
+      {"id":42,"itemName":"s3:object-lock-remaining-retention-days"},
+      {"id":43,"itemName":"s3:object-lock-retain-until-date"},
+      {"id":44,"itemName":"s3:prefix"},
+      {"id":45,"itemName":"s3:signatureage"},
+      {"id":46,"itemName":"s3:signatureversion"},
+      {"id":47,"itemName":"s3:versionid"},
+      {"id":48,"itemName":"s3:x-amz-acl"},
+      {"id":49,"itemName":"s3:x-amz-content-sha256"},
+      {"id":50,"itemName":"s3:x-amz-copy-source"},
+      {"id":51,"itemName":"s3:x-amz-grant-full-control"},
+      {"id":52,"itemName":"s3:x-amz-grant-read"},
+      {"id":53,"itemName":"s3:x-amz-grant-read-acp"},
+      {"id":54,"itemName":"s3:x-amz-grant-write"},
+      {"id":55,"itemName":"s3:x-amz-grant-write-acp"},
+      {"id":56,"itemName":"s3:x-amz-metadata-directive"},
+      {"id":57,"itemName":"s3:x-amz-server-side-encryption"},
+      {"id":58,"itemName":"s3:x-amz-server-side-encryption-aws-kms-key-id"},
+      {"id":59,"itemName":"s3:x-amz-storage-class"},
+      {"id":60,"itemName":"s3:x-amz-website-redirect-location"}
+    ];
+
+    this.dropdownConditionKeySettings = {
+  		singleSelection: true,
+  		text:"Select Condition Key",
+  		selectAllText:'Select All',
+  		unSelectAllText:'UnSelect All',
+  		enableSearchFilter: true
+	  };
   }
 
-	onItemSelect(item:any){
+	onActionItemSelect(item:any){
 	    console.log(item);
-	    console.log(this.selectedItems);
+	    console.log(this.selectedActions);
 	}
-	OnItemDeSelect(item:any){
+	onActionItemDeSelect(item:any){
 	    console.log(item);
-	    console.log(this.selectedItems);
+	    console.log(this.selectedActions);
 	}
-	onSelectAll(items: any){
+	onActionSelectAll(items: any){
 	    console.log(items);
 	}
-	onDeSelectAll(items: any){
+	onActionDeSelectAll(items: any){
 	    console.log(items);
 	}
+
+
+  //condition select actions
+  onConditionItemSelect(item:any){
+	    console.log(item);
+	    console.log(this.selectedConditions);
+	}
+	onConditionItemDeSelect(item:any){
+	    console.log(item);
+	    console.log(this.selectedConditions);
+	}
+	onConditionSelectAll(items: any){
+	    console.log(items);
+	}
+	onConditionDeSelectAll(items: any){
+	    console.log(items);
+	}
+
+  //condition key select actions
+  onConditionKeyItemSelect(item:any){
+      console.log(item);
+      console.log(this.selectedConditionsKey);
+  }
+  onConditionKeyItemDeSelect(item:any){
+      console.log(item);
+      console.log(this.selectedConditionsKey);
+  }
+  onConditionKeySelectAll(items: any){
+      console.log(items);
+  }
+  onConditionKeyDeSelectAll(items: any){
+      console.log(items);
+  }
+
 
   searchItems() {
     console.log(this.searchText)
@@ -146,22 +329,24 @@ export class PoliciesComponent implements OnInit {
 
   private resetPloicyForm(removeName){
   	console.log(removeName)
-  	this.selectedItems = []
+  	this.selectedActions = []
   	if(!removeName){
   		this.newPolicy.effect = "Allow"
-		this.newPolicy.bucket = ""
+		  this.newPolicy.bucket = ""
   	}else{
   		this.newPolicy.name = "",
-		this.newPolicy.effect = "Allow"
-		this.newPolicy.bucket = ""
+  		this.newPolicy.effect = "Allow"
+  		this.newPolicy.bucket = ""
   	}
 
     this.newStatement = {
       Action: [],
       Effect: "",
-      Resource: []
+      Resource: [],
+      Condition: {},
+      Principal: ""
     }
-  	
+
   }
 
   private getPolicies(){
@@ -208,20 +393,60 @@ export class PoliciesComponent implements OnInit {
   	}
   }
 
+  private removeCondition(valueId,keyName,conditionName){
+    console.log(this.newStatement)
+    console.log(valueId,keyName,conditionName)
+    this.newStatement.Condition[conditionName][keyName].splice(valueId,1)
+  }
+
+  private addCondition(){
+    console.log(this.selectedCondition[0].itemName)
+    console.log(this.selectedConditionKey[0].itemName)
+    console.log(this.newConditionValue)
+    if(!this.newStatement.Condition){
+      this.newStatement.Condition = {}
+    }
+    if(this.newStatement.Condition[this.selectedCondition[0].itemName]){
+      if(this.newStatement.Condition[this.selectedCondition[0].itemName][this.selectedConditionKey[0].itemName]){
+        this.newStatement.Condition[this.selectedCondition[0].itemName][this.selectedConditionKey[0].itemName].push(this.newConditionValue)
+      }else{
+        this.newStatement.Condition[this.selectedCondition[0].itemName][this.selectedConditionKey[0].itemName] = []
+        this.newStatement.Condition[this.selectedCondition[0].itemName][this.selectedConditionKey[0].itemName].push(this.newConditionValue)
+      }
+    }else{
+      this.newStatement.Condition[this.selectedCondition[0].itemName] = {}
+      this.newStatement.Condition[this.selectedCondition[0].itemName][this.selectedConditionKey[0].itemName] = []
+      this.newStatement.Condition[this.selectedCondition[0].itemName][this.selectedConditionKey[0].itemName].push(this.newConditionValue)
+    }
+
+    this.selectedCondition = ""
+    this.selectedConditionKey = ""
+    this.newConditionValue = ""
+    console.log(this.newStatement.Condition)
+  }
 
 
   private addStatement(){
 
-  	if(this.selectedItems.length == this.dropdownList.length){
+  	if(this.selectedActions.length == this.dropdownActionList.length){
   		this.newStatement.Action.push("s3:*")
   	}else{
-	  	for (var i = 0; i < this.selectedItems.length; i++) {
-	  		this.newStatement.Action.push(this.selectedItems[i].itemName)
+	  	for (var i = 0; i < this.selectedActions.length; i++) {
+	  		this.newStatement.Action.push(this.selectedActions[i].itemName)
 	  	}
 	  }
   	this.newStatement.Effect = this.newPolicy.effect
   	// this.newStatement.Resource = "arn:aws:s3:::"+this.newPolicy.bucket
   	console.log(this.newStatement)
+    if(this.newStatement.Condition && Object.entries(this.newStatement.Condition).length === 0 && this.newStatement.Condition.constructor === Object){
+      console.log("Condition removed cause empty")
+      delete this.newStatement.Condition
+    }else{
+      if(!this.newStatement.Principal || !this.newStatement.Principal == ""){
+        console.log("Principal set to * cause condition not empty")
+        this.newStatement.Principal = "*"
+      }
+    }
 
   	this.newPolicyRaw.Statement.push(this.newStatement);
   	console.log(this.newPolicyRaw)
@@ -233,10 +458,10 @@ export class PoliciesComponent implements OnInit {
     this.newStatement = this.newPolicyRaw.Statement[i]
     this.newPolicy.effect = this.newPolicyRaw.Statement[i].Effect
     if(this.newStatement.Action[0] == "s3:*"){
-        this.selectedItems = this.dropdownList
+        this.selectedActions = this.dropdownActionList
     }else{
       for (var g = 0; g < this.newStatement.Action.length; g++) {
-        this.selectedItems.push({"id":g,"itemName":this.newStatement.Action[g]})
+        this.selectedActions.push({"id":g,"itemName":this.newStatement.Action[g]})
       }
     }
     this.newStatement.Action = []
